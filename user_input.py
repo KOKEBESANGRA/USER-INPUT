d11f5edfec72d6518c540ba78ac8f0bd80db434c
KOKEBESANGRA <herleedkokebe@gmail.com>
2024-10-31 00:30:05 +0300

    PYTHON ASSIGNMENT WEEK 1

---
+++ user_input.py
@@ -0,0 +1,7 @@
+# Get user input for name, age, and location
+name = input("Please enter your name: ")
+age = input("Please enter your age: ")
+location = input("Please enter your location: ")
+
+# Print personalized message
+print(f"Hello {name}, you are {age} years old and live in {location}.")
